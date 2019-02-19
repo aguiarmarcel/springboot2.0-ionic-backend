cad2539db1ca4ecc1e608f32d4da16f57938c0b4
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2019-02-19 19:32:58 -0300

    Projeto Prof. Nelio até a aula 99(ajustes fonais no backend e bucket) devido a troca de HD.

--- src/main/java/com/nelioalves/cursomc/services/EmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.nelioalves.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.nelioalves.cursomc.domain.Cliente;
@@ -11,5 +13,9 @@ public interface EmailService {
 	
 	void sendEmail(SimpleMailMessage msg);
 	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
+
 	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
